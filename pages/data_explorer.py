e4afb3dea00404f7eb0eebd864b70d90e8334dc7
AhmedUdst <156051758+AhmedUdst@users.noreply.github.com>
2025-04-07 00:08:59 +0300

    Update data_explorer.py

--- pages/data_explorer.py
+++ pages/data_explorer.py
@@ -11,7 +11,7 @@ st.set_page_config(page_title="📊 Free Time Data Explorer", layout="wide")
 st.title("📊 Data Exploration Dashboard")
 
 # Load dataset from GitHub
-github_url = "https://raw.githubusercontent.com/AhmedUdst/ai_mena/main/clean_dataset.csv"
+github_url = "https://raw.githubusercontent.com/AhmedUdst/ai_mena/main/dataset.csv"
 
 try:
     response = requests.get(github_url)
